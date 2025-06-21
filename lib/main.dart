441e32e872eb1e1e5bbbc6daa41cf22f808c62f9
Ezra Cortez <lehi170993@gmail.com>
2025-06-20 19:01:33 -0600

    feat:giro automatico para tablet

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+//import 'package:device_info_plus/device_info_plus.dart';
+
 import 'controllers/theme_controller.dart';
 import 'screens/home_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Detectar ancho físico en dp
+  final widthInDp = WidgetsBinding
+          .instance.platformDispatcher.views.first.physicalSize.width /
+      WidgetsBinding.instance.platformDispatcher.views.first.devicePixelRatio;
+
+  final isTablet = widthInDp >= 600; // criterio estándar Android
+  debugPrint('Ancho en dp: $widthInDp — ¿Es tablet? $isTablet');
+
+  if (isTablet) {
+    await SystemChrome.setPreferredOrientations([
+      //DeviceOrientation.portraitUp,
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+  } else {
+    await SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+  }
+
   runApp(
     ChangeNotifierProvider(
       create: (_) => ThemeController(),
@@ -11,6 +36,7 @@ void main() {
     ),
   );
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
