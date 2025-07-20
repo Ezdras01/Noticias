c8b2d070ba6f1c20c85fe88679900d41f80ece84
Ezra Cortez <lehi170993@gmail.com>
2025-07-20 17:37:54 -0600

    feat: lógica del home
    con esta lógica se pretende hacer el enrutamiento para los diseños de las diferentes pantallas

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -11,16 +11,23 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     final shortestSide = size.shortestSide;
+    // es una tablet si es mayor o igual a 600 px y menor a 900 px
     final isTablet = shortestSide >= 600 && shortestSide < 900;
+    // es desktop si es mayor o igual a 900 px
     final isDesktop = shortestSide >= 900;
+    // determina si la orientación es vertical o horizontal
     final isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
 
+// si es difrente de tablet y difrente de desktop, entonces es phone
     if (!isTablet && !isDesktop) {
       return const HomePhone();
+      // si es una tablet y esta en modo vertical
     } else if (isTablet && isPortrait) {
       return const HomeTabletVertical();
+      // si es una tablet y esta en modo horizontal
     } else if (isTablet && !isPortrait) {
       return const HomeTabletHorizontal();
+      // para todo lo demás es desktop
     } else {
       return const HomeDesktop();
     }
